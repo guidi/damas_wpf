481586dbe7591a404735f8ca8aaf71ef9d4ce5bc
Vanderson Guidi <vando.lg@gmail.com>
2024-05-19 22:20:06 -0300

    Commit inicial - criando o projeto e desenhando o tabuleiro.

---
+++ src/Damas/DamasWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DamasWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ src/Damas/DamasWPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DamasWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DesenharTabuleiro();
+        }
+
+        private void DesenharTabuleiro()
+        {
+            int linhas = 8;
+            int colunas = 8;
+            SolidColorBrush quadradoEscuro = new SolidColorBrush(Color.FromRgb(139, 69, 19));  //marrom
+            SolidColorBrush quadradoClaro = new SolidColorBrush(Color.FromRgb(245, 222, 179)); //bege
+
+            for (int row = 1; row <= linhas; row++)
+            {
+                for (int col = 1; col <= colunas; col++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = (row + col) % 2 == 0 ? quadradoClaro : quadradoEscuro; //Para criar o padrão alternado nas cores
+                    Grid.SetRow(rect, row);
+                    Grid.SetColumn(rect, col);
+                    Tabuleiro.Children.Add(rect);
+                }
+            }
+        }
+    }
+}
