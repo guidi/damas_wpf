bba8fdcb375d18c1aa43073fbb60812b45656499
Vanderson Guidi <vando.lg@gmail.com>
2024-05-21 23:05:27 -0300

    Incluindo as peças no tabuleiro.

--- src/Damas/DamasWPF/MainWindow.xaml.cs
+++ src/Damas/DamasWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace DamasWPF
@@ -20,20 +21,47 @@ namespace DamasWPF
         {
             int linhas = 8;
             int colunas = 8;
-            SolidColorBrush quadradoEscuro = new SolidColorBrush(Color.FromRgb(139, 69, 19));  //marrom
-            SolidColorBrush quadradoClaro = new SolidColorBrush(Color.FromRgb(245, 222, 179)); //bege
+            SolidColorBrush quadradoEscuro = new SolidColorBrush(Color.FromRgb(139, 69, 19));
+            SolidColorBrush quadradoClaro = new SolidColorBrush(Color.FromRgb(245, 222, 179));
+            SolidColorBrush borda = new SolidColorBrush(Color.FromRgb(59, 42, 24)); 
 
             for (int row = 1; row <= linhas; row++)
             {
                 for (int col = 1; col <= colunas; col++)
                 {
-                    Rectangle rect = new Rectangle();
-                    rect.Fill = (row + col) % 2 == 0 ? quadradoClaro : quadradoEscuro; //Para criar o padrão alternado nas cores
-                    Grid.SetRow(rect, row);
-                    Grid.SetColumn(rect, col);
-                    Tabuleiro.Children.Add(rect);
+                    Border border = new Border
+                    {
+                        Background = (row + col) % 2 == 0 ? quadradoClaro : quadradoEscuro,
+                        BorderThickness = new Thickness(1),
+                        BorderBrush = borda
+                    };
+
+                    Grid.SetRow(border, row);
+                    Grid.SetColumn(border, col);
+                    Tabuleiro.Children.Add(border);
+
+                    if ((row + col) % 2 != 0 && (row < 4 || row > 5)) // Regrinha pra adicionar as peças nos lugares certos.
+                    {
+                        string nomeImagemPeca = ObterNomeImagemPeca(row);
+                        if (!string.IsNullOrEmpty(nomeImagemPeca))
+                        {
+                            Image img = new Image
+                            {
+                                Source = new BitmapImage(new Uri($"pack://application:,,,/assets/{nomeImagemPeca}.png", UriKind.Absolute)),
+                                Width = 60,
+                                Height = 60
+                            };
+
+                            border.Child = img;
+                        }
+                    }
                 }
             }
+        }
+
+        private string ObterNomeImagemPeca(int row)
+        {
+            return (row < 4) ? "branca" : "vermelha";
         }
     }
 }
